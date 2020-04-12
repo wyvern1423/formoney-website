1bcb9f3c223c0899699448131c552079a5ab167f
wyvern1423 <xiaoxun1208@126.com>
2020-04-12 17:46:34 +0800

    deploy

---
+++ precache-manifest.297a3a75f4eb331fb49b50a3bda9e57c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbd5e7f093e22051082",
+    "url": "/formoney-website/css/app.0c44f3b1.css"
+  },
+  {
+    "revision": "4527dd75f76bb22de7b134fefb7cb7b8",
+    "url": "/formoney-website/index.html"
+  },
+  {
+    "revision": "ddbd5e7f093e22051082",
+    "url": "/formoney-website/js/app.b69435dd.js"
+  },
+  {
+    "revision": "f6d3dd836a14d7830e70",
+    "url": "/formoney-website/js/chunk-vendors.f1347ba1.js"
+  },
+  {
+    "revision": "e11b5abc45be7ad068eabd81d89f3961",
+    "url": "/formoney-website/manifest.json"
+  },
+  {
+    "revision": "85d5513bc09e708d940a6584b5705b78",
+    "url": "/formoney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/formoney-website/robots.txt"
+  }
+]);
